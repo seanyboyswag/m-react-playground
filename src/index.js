d09706a4929a021a70c85b2ed793e8814a773da3
seanyboyswag <2150218@slu.edu.ph>
2020-03-14 22:02:10 +0800

    Added text changer

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 const App = (props) => {
     const [count, setCount] = useState(props.count);
+    const [text, setText] = useState('');
 
     return (
         <div>
-            <p>The current count is: {count}</p>
+            <p>The current {text || 'count'} is: {count}</p>
             <button onClick={() => setCount(count + 1)}>-1</button>
             <button onClick={() => setCount(props.count)}>Reset</button>
             <button onClick={() => setCount(count + 1)}>+1</button>
+            <input value={text} onChange={(e) => setText(e.target.value) }/>
         </div>
     )
 }
